b4279dce64352987a2988a0b2990876eda7ec8bb
Karl S <gokaertle@gmx.de>
2019-03-06 23:04:59 +0100

    delete output messages for batch processing

--- FileCut.java
+++ FileCut.java
@@ -13,72 +13,78 @@ public class FileCut {
 	final static String ARG_DEL_TO_START = "deleteToStartOf";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("FileCut running... In:"+args[0]+" Out:"+args[1]+" Mode:"+args[2]+" Pattern:"+args[3]);
-		
-		InputStream inStream = null;
-        OutputStream outStream = null;
-        
-        int mode;
-        switch (args[2]) {
-        case ARG_DEL_TO_START: mode=MODE_DEL_TO_START; break;
-        default: mode = 0;
-        }
-        if (args[0].length() == 0) {
-        	System.out.println("no input file given!");
-        	return;
-        }
-        if (args[1].length() == 0) {
-        	System.out.println("no output file given!");
-        	return;
-        }
-        if (args[2].length() == 0) {
-        	System.out.println("no mode given!");
-        	return;
-        }
-        if (args[3].length() == 0) {
-        	System.out.println("no pattern given!");
-        	return;
-        }
-        String pattern = args[3];
-        if (mode!=0) {
-	        try {
-	            File afile = new File(args[0]);
-	            File bfile = new File(args[1]);
-	
-	            inStream = new FileInputStream(afile);
-	            outStream = new FileOutputStream(bfile);
-	
-	            BufferByte patBuf = new BufferByte(pattern.length());
+				InputStream inStream = null;
+				OutputStream outStream = null;
+				
+				if (args.length < 4) {
+					System.out.println("not enough parameter given!");
+					System.out.println("Usage:");
+					System.out.println("FileCut <inputFile> <outputFile> deleteToStartOf <pattern>");
+					return;
+				}
+						
+				// System.out.println("FileCut running... In:"+args[0]+" Out:"+args[1]+" Mode:"+args[2]+" Pattern:"+args[3]);
 
-	            int charRead;
-	            boolean writeFlag = false;
-	            // copy the file content in bytes
-	            while ((charRead = inStream.read()) > 0)
-	            {
-	            	patBuf.push((byte)charRead);
-	            	
-	            	if (patBuf.toString().equals(pattern)) {
-	            		writeFlag = true;
-	            		
-	            		// write content of Buffer to file
-	            		outStream.write(patBuf.toBytes());
-	            	} else { // use else to not write when written buffer due to char duplication
-	            		if (writeFlag) {
-	            			outStream.write(charRead);
-	            		}
-	            	}
-	            }	
-	            inStream.close();
-	            outStream.close();	
-	            System.out.println("File is copied successful!");
-	        }
-	        catch (IOException e) {
-	            e.printStackTrace();
-	        }
-        } else {
-        	System.out.println("invalid mode given!");
-        }
+				int mode;
+				switch (args[2]) {
+				case ARG_DEL_TO_START: mode=MODE_DEL_TO_START; break;
+				default: mode = 0;
+				}
+				if (args[0].length() == 0) {
+					System.out.println("no input file given!");
+					return;
+				}
+				if (args[1].length() == 0) {
+					System.out.println("no output file given!");
+					return;
+				}
+				if (args[2].length() == 0) {
+					System.out.println("no mode given!");
+					return;
+				}
+				if (args[3].length() == 0) {
+					System.out.println("no pattern given!");
+					return;
+				}
+				String pattern = args[3];
+				if (mode!=0) {
+					try {
+							File afile = new File(args[0]);
+							File bfile = new File(args[1]);
+	
+							inStream = new FileInputStream(afile);
+							outStream = new FileOutputStream(bfile);
+	
+							BufferByte patBuf = new BufferByte(pattern.length());
+
+							int charRead;
+							boolean writeFlag = false;
+							// copy the file content in bytes
+							while ((charRead = inStream.read()) > 0)
+							{
+								patBuf.push((byte)charRead);
+								
+								if (patBuf.toString().equals(pattern)) {
+									writeFlag = true;
+									
+									// write content of Buffer to file
+									outStream.write(patBuf.toBytes());
+								} else { // use else to not write when written buffer due to char duplication
+									if (writeFlag) {
+										outStream.write(charRead);
+									}
+								}
+							}	
+							inStream.close();
+							outStream.close();	
+							// System.out.println("File is copied successful!");
+					}
+					catch (IOException e) {
+							e.printStackTrace();
+					}
+				} else {
+					System.out.println("invalid mode given!");
+				}
 
 	}
 
@@ -95,7 +101,7 @@ class BufferByte {
 	}
 	
 	public void push(byte c) {
-		System.out.println(toString()+" pos"+pos+" max"+max+" c"+c);
+		//System.out.println(toString()+" pos"+pos+" max"+max+" c"+c);
 		if (pos < max) {
 			buffer[pos] = c;
 			pos++;
